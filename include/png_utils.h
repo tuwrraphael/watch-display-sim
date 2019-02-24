dfb6a8e62e234dcd4d7270a37ddf0365ce2c9b8b
Raphael <raphael.hauk@racing.tuwien.ac.at>
2019-02-24 03:00:24 +0100

    running transform

--- include/png_utils.h
+++ include/png_utils.h
@@ -1,9 +1,11 @@
 #include "png.h"
 
-typedef struct {
+typedef struct
+{
     int width;
-    int
-}
+    png_bytep *row_pointers;
+    int height;
+} display_png_t;
 
-void write_png_file(char *filename, png_bytep *row_pointers);
-png_bytep *read_png_file(char *filename);
+void write_png_file(char *filename, display_png_t *displayPng);
+void read_png_file(char *filename, display_png_t *displayPng);
--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 
 #include "png_utils.h"
 
-void process_png_file(png_bytep *row_pointers)
+void process_png_file(display_png_t *displayPng)
 {
   for (int y = 0; y < 128; y++)
   {
-    png_bytep row = row_pointers[y];
+    png_bytep row = displayPng->row_pointers[y];
     for (int x = 0; x < 128; x++)
     {
       png_bytep px = &(row[x * 4]);
-
+      px[0] = 15;
       // Do something awesome for each pixel here...
       //printf("%4d, %4d = RGBA(%3d, %3d, %3d, %3d)\n", x, y, px[0], px[1], px[2], px[3]);
     }
@@ -21,8 +21,9 @@ void process_png_file(png_bytep *row_pointers)
 
 int main(int argc, char *argv[])
 {
-  png_bytep *row_pointers = read_png_file("img/empty.png");
-  process_png_file(row_pointers);
-  write_png_file("out.png", row_pointers);
+  display_png_t png;
+  read_png_file("img/empty.png", &png);
+  process_png_file(&png);
+  write_png_file("out.png", &png);
   return 0;
 }
