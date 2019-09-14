03f237f4d64e728e25200d9a2fa89d32a369c165
Raphael <raphael.hauk@racing.tuwien.ac.at>
2019-09-14 02:31:58 +0200

    update dependencies

---
+++ generate-assets.ps1
@@ -0,0 +1,2 @@
+node ./img-generator/index.js --dir 'digit-watch-ui/assets/' --prefix icon --header --out 'digit-watch-ui/include/digit_assets.h'
+node ./img-generator/index.js --dir 'digit-watch-ui/assets/' --prefix icon --out 'digit-watch-ui/src/digit_assets.c'
